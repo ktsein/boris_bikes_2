39250dc2b20fd1960e055091ede549c74076a3f9
Malinna Leach <malinnab@yahoo.com>
2016-09-29 14:41:15 +0100

    adding predicate methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,14 +6,25 @@ class DockingStation
   end
 
   def release_bike
-    raise "sorry no bikes available" if @bikes.length == 0
+    raise "sorry no bikes available" if empty?
     return @bikes.pop
   end
 
   def dock_bike(bikes)
-    fail "sorry this station is full" if @bikes.length >= 20
+    fail "sorry this station is full" if full?
     @bikes.push bikes
   end
+
+  private
+
+  def full?
+    @bikes.length == 20
+  end
+
+  def empty?
+    @bikes.length == 0
+  end
+
 end
 
 class Bike
