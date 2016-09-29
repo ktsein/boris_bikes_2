bd1b9251eef825cf4f17fe5287f61633642fe13d
Malinna Leach <malinnab@yahoo.com>
2016-09-29 17:35:32 +0100

    applied some double behaviour for :bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,13 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+
     it {is_expected.to respond_to :release_bike}
+
+    let(:bike) {double :bike}
     it 'releases previously docked bike' do
       bike = double(:bike) #refer to class
+      allow(bike).to receive(:working).and_return(true)
       subject.dock_bike(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -31,9 +35,12 @@ describe DockingStation do
       expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
     end
 
+    let(:bike) {double :bike}
     it 'will not release a bike if it\'s broken' do
       bike = double(:bike)#refer to class
-      subject.dock_bike(bike, 'broken')
+      allow(bike).to receive(:working).and_return(false)
+      subject.dock_bike(bike)
+      #allow(bike).to receive(:working).and_return(false)
       expect {subject.release_bike}.to raise_error("This bike is faulty")
     end
 
